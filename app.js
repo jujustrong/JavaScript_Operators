b7db39d9a01d5436cbf0174bf43b2772d11fae78
Julian Armstrong <julian.armstrong8@gmail.com>
2024-10-20 14:13:42 -0400

    Completed Exercises and printed answers to the console

--- app.js
+++ app.js
@@ -6,14 +6,49 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+var a = 20;
+var b = 4;
+var add = a+b;
+var minus = a-b;
+var multiply = a*b;
+var dividing = a/b;
+
+console.log(add, minus, multiply, dividing)
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(typeof(num+str), num+str );
+console.log(typeof(num+str2), num+str2);
+console.log(typeof(num+isPresent) , num+isPresent);
+console.log(typeof(firstName+num), firstName+num);
+console.log(typeof(isPresent+str), isPresent+str);
+console.log(typeof(firstName+lastName), firstName+lastName);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(val == str3);
+console.log(val === str3);
+console.log(!isAwake);
+console.log("eleven" == str4 && val >= str3);
+console.log(!isAwake || isAwake);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
+
+
